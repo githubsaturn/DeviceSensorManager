feaa49b5d5685c72a087d35cc8007c6112e8593d
Kasra <kasra85@gmail.com>
2015-02-11 10:12:23 -0800

    Initial Release
    Only lists the sensors and counts them

---
+++ app/src/main/java/com/bigdeli/kasra/devicesensormanager/AppConfig.java
@@ -0,0 +1,14 @@
+package com.bigdeli.kasra.devicesensormanager;
+
+/**
+ * Created by kbigdeli on 2/10/2015.
+ */
+public class AppConfig {
+    public static boolean IS_DEBUG_ON = true;
+    public static String getClassName(Object o){
+        String fullClassName = o.getClass().getName();
+        int lastDot = fullClassName.lastIndexOf(".");
+        String localClassName = fullClassName.substring(lastDot);
+        return localClassName;
+    }
+}
